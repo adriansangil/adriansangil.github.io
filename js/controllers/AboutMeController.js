f656c21bf3c798083ada1f1cfe042a4068d49107
adriansangil <adz.san182@gmail.com>
2018-02-25 16:18:23 -0800

    changed index to contain info from aboutMeController

--- js/controllers/AboutMeController.js
+++ js/controllers/AboutMeController.js
@@ -1,4 +1,14 @@
 app.controller('AboutMeController', ['$scope', function($scope) { 
   $scope.greetings1 = "Hello,";
   $scope.greetings2 = "a bit about me:";
+
+  let myInfo = [];
+
+  myInfo.push({title:"Wise Words",description:"The expert in anything was once a beginner."});
+  myInfo.push({title:"Hobbies",description:"Playing the guitar, reading manga, sleeping and eating."});
+  myInfo.push({title:"Web Developer",description:"I create web pages and web-based systems based on the client."});
+  myInfo.push({title:"Gamer",description:"Gamers don't die, We respawn.", gameLink:"#", gameMessage:"Add me on Steam"});
+
+  $scope.aboutMe = myInfo;
+
 }]);
