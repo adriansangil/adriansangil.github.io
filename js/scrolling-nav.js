76ecd912ec614b64c4a586bfbc89dc5e99e48f2f
adriansangil <adz.san182@gmail.com>
2018-03-01 22:04:04 -0800

    fix menu nav not collapsing after selection in mobile view

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -17,3 +17,9 @@ $(function() {
         event.preventDefault();
     });
 });
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a:not(".dropdown-toggle")') ) {
+        $(this).collapse('hide');
+    }
+});
