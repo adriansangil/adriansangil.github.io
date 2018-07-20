a61ddb48cc41543f107530c263336221bc03355a
Adrian <adz.san182@gmail.com>
2018-07-19 21:48:15 -0700

    updated skill section

--- js/controllers/SkillsController.js
+++ js/controllers/SkillsController.js
@@ -2,10 +2,10 @@ app.controller('SkillsController', ['$scope', function($scope) {
    $scope.header = "Skills and Technology";
 
   let mySkills = {
-    web:" CSS, HTML, Javascript, jQuery, SQL, Bootstrap, REST Architecture, Git",
-    lang: " Java, J2EE, PHP",
+    web:" CSS, HTML, Javascript, jQuery, SQL, Bootstrap 3, REST Architecture, Git",
+    lang: " Java, J2EE, PHP, familiar with Python",
     mobile: " familiar with Android and Unity",
-    others: " Spring MVC, Spring Boot, Maven, Jmeter, familiar with NodeJS, AngularJS, CodeIgniter, JUnit, Mockito"
+    others: " Spring MVC, Spring Boot, Maven, Jmeter, familiar with NodeJS, AngularJS, CodeIgniter, JUnit, Mockito, UNIX scripting"
   }
   $scope.skills = mySkills;
 
